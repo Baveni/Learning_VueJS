df88c1af5f756d5e8e807be6b38c0c71e1a385b4
Baveni <uranic.jernej@gmail.com>
2019-07-21 20:21:21 +0200

    lesson two

---
+++ app_two.js
@@ -0,0 +1,18 @@
+new Vue({
+        el: '#exercise',
+        data: {
+            value: ''
+        },
+
+        methods: {
+
+            alertMe: function() {
+                alert('this is alert');
+            },
+
+            storeValue: function(event) {
+                this.value = event.target.value;
+            }
+
+        },
+    });
