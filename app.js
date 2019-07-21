d9122b4f41e349990ca55bbb97289e280d9a86da
Baveni <uranic.jernej@gmail.com>
2019-07-21 11:51:26 +0200

    first lesson completed

--- app.js
+++ app.js
@@ -2,28 +2,27 @@ new Vue({
     el: '#exercise',
     data: {
         MyName: 'what is your name? ',
-        MyAge: 33,
+        MyAge: 19,
+        
     },
+    /*
+    functions syntay:
+    name: func() {},
+    name2: func() {},
+    */
     methods: {
         changeName: function(event) {
             this.MyName = event.target.value;
         },
-    
-     
+
         outputAge: function(event) {
             return this.MyAge * 3
         },
-    
-/*     methods: {
-        ageMulti: function () {
-            return this.MyAge * 3
-        }
-    },
-    methods: {
-        getRandomFloat: function () {
-            var max = 3
-            return Math.floor(Math.random(Math.round) * Math.floor(max) )
-        }
-    } */
+
+        getRandomFloat: function() {
+            /*return Math.floor(Math.random(Math.round(max_int)))*/
+            return Math.random()
+        },
+
 }});
 
